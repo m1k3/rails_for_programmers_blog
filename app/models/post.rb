1439edaaf15fabb1cfd1b4fc489975dbd0a72026
Michal Olah <olahmichal@gmail.com>
2014-05-03 13:43:09 +0200

    hiding edit links from non-authors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   scope :ordered, -> { order('created_at DESC') }
+
+  def author?(current_user)
+    user == current_user
+  end
 end
